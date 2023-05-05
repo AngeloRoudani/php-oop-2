91cd326a6d5d35c60dd9a09948206158acdd30ce
AngeloRoudani <ange.rouda@gmail.com>
2023-05-05 17:57:58 +0200

    modifiche style

--- data/dataProducts.php
+++ data/dataProducts.php
@@ -1,5 +1,9 @@
 <?php 
 
 
-$flea = new CatProduct('StopPulci', './img/antipulci.jpg', '34.99 €', 'Medicinale','Adulti', './img/cat.jpg');
-$ball = new DogProduct('Pallina','./img/pallina_cane.webp', '5.99 €', 'Giocattolo', 'Ogni Età', './img/dog.png');
+$flea = new CatProduct('StopPulci', './img/antipulci.jpg', '34.99 €', 'Medicinale','Adulti');
+$flea->getKind('./img/cat.jpg');
+
+
+$ball = new DogProduct('Pallina','./img/pallina_cane.webp', '5.99 €', 'Giocattolo', 'Ogni Età');
+$ball->getKind('./img/dog.png');
