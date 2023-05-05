feafdb66c1ee938daeae221918e02cbbc4ebc69f
AngeloRoudani <ange.rouda@gmail.com>
2023-05-05 18:00:11 +0200

    spostamento prodotti in dataProducts

--- index.php
+++ index.php
@@ -4,13 +4,8 @@
 require __DIR__.'/models/AnimalProduct.php';
 require __DIR__.'/models/DogProduct.php';
 require __DIR__.'/models/CatProduct.php';
+require __DIR__.'/data/dataProducts.php';
 
-$flea = new CatProduct('StopPulci', './img/antipulci.jpg', '34.99 €', 'Medicinale','Adulti');
-$flea->getKind('./img/cat.jpg');
-
-
-$ball = new DogProduct('Pallina','./img/pallina_cane.webp', '5.99 €', 'Giocattolo', 'Ogni Età');
-$ball->getKind('./img/dog.png');
 
 ?>
 
